4288444ea6962da946c2f9dd17e7af143473c149
Wayne <grigswa@gmail.com>
2019-08-20 09:57:34 -0400

    Update file
    Generalized file and removed django specific code.

--- analyze_text.py
+++ analyze_text.py
@@ -1,11 +1,10 @@
 import argparse
-
 from django.conf import settings
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 import pandas as pd
-from scrape.models import Yelp, Results, Analytics
+from scrape.models import Yelp, Results
 import spacy
 from spacy.lang.en import English
 
@@ -15,11 +14,10 @@ parser = English()
 
 
 class AnalyzeText:
-    def __init__(self, instance, instance_id):
-        self.biz_slug = instance.slug
+    def __init__(self, biz_slug):
+        self.biz_slug = biz_slug
         self.words_ignore = self.gen_ignore_words()
         self.originData = self.gather_data()
-        # self.words = self.clean_data()
 
     def clean_data(self):
         """This merges all reviews into a single string and removes all stop
@@ -64,57 +62,30 @@ class AnalyzeText:
 
 
 class ProcessCommon(AnalyzeText):
-    def __init__(self, instance, instance_id, *args):
-        super().__init__(instance, instance_id,)
+    def __init__(self, biz_slug, *args):
+        super().__init__(biz_slug)
         self.words = self.clean_data()
-        self.results = None
-        self.instance_id = instance_id
 
     def analyze_data(self):
         """word frequencies are generated and are compared against word lengths"""
         words = nltk.FreqDist(self.words)
-        most_common = words.most_common(50)
+        most_common = [x[0] for x in words.most_common(50)]
 
         # sort words by the length of word and then by the number of occurrences in the doc
         # most_common_alt = list(w for w in set(self.words) if len(w) >= 5 and words[w] >= 5)
-        # most_common_alt = list(w for w in set(self.words) if len(w) >= 5 and w in most_common)
-        self.results = most_common
-        # print(len(most_common), most_common)
-        # print(len(most_common_alt), most_common_alt)
-
-    def upload_results(self):
-        for result in self.results:
-            # add some custom validation\parsing for some of the fields
-            yelp_obj = Yelp.objects.get(id=self.instance_id)
-            instance = Analytics(business=yelp_obj, word=result[0], value=result[1])
-            try:
-                instance.save()
-            except:
-                pass
+        most_common_alt = list(w for w in set(self.words) if len(w) >= 5 and w in most_common)
+        print(len(most_common), most_common)
+        print(len(most_common_alt), most_common_alt)
 
     def run(self):
         self.analyze_data()
-        self.upload_results()
 
 
-# class ProcessSentiment(AnalyzeText):
+# class ProcessSentiment(AnalyzeText): (UNDER DEV)
 #     def __init__(self, biz_slug, *args):
 #         super().__init__(biz_slug)
 #         self.words = self.clean_data()
-#
-#     def analyze_data(self):
-#         """word frequencies are generated and are compared against word lengths"""
-#         words = nltk.FreqDist(self.words)
-#         most_common = [x[0] for x in words.most_common(50)]
-#
-#         # sort words by the length of word and then by the number of occurrences in the doc
-#         # most_common_alt = list(w for w in set(self.words) if len(w) >= 5 and words[w] >= 5)
-#         most_common_alt = list(w for w in set(self.words) if len(w) >= 5 and w in most_common)
-#         print(len(most_common), most_common)
-#         print(len(most_common_alt), most_common_alt)
-#
-#     def run(self):
-#         self.analyze_data()
+
 
 def main():
     parser = argparse.ArgumentParser(description='business slug')
@@ -129,12 +100,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
